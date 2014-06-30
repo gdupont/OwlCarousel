e414905b4a134ed54b191bc9a2cd5a95b28e5698
Guilherme Dupont <guilhermedupont@gmail.com>
2014-06-30 17:49:27 -0300

    fix lazy load

--- owl-carousel/owl.carousel.js
+++ owl-carousel/owl.carousel.js
@@ -1210,7 +1210,7 @@ if (typeof Object.create !== "function") {
             function checkLazyImage( $img ) {
                 iterations += 1;
                 if (base.completeImg($img.get(0)) || isBackgroundImg === true) {
-                    showImage();
+                    showImage( $img );
                 } else if (iterations <= 100) {//if image loads in less than 10 seconds
                     window.setTimeout(checkLazyImage($img) , 100);
                 } else {
